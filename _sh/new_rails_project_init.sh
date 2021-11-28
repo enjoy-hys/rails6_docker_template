1a743a3ba8789eec704c3d2eb0f16b6bc2f8287c
Jay <i-hayashi@starx.co.jp>
2021-11-28 22:57:35 +0900

    add init Rails6 stem to shell script

--- _sh/new_rails_project_init.sh
+++ _sh/new_rails_project_init.sh
@@ -1,1 +1,8 @@
+## 初始化 Rails 项目
+- 项目第一次启动时, Rails 还未安装，目录下的 Gemfile 只有 rails, Gemfile.lock 为空白
 docker-compose run --rm web rails new . --force --no-deps --database=mysql
+docker-compose build
+sed -i -e "s/host:.*/host: db/" config/database.yml
+docker-compose run --rm web rake db:create
+## 执行完以上所有命令后，才可以用以下的命令成功启动初始化的 Rails 项目
+# docker-compose up
